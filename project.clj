c89bfad428c0ba520647412df275a31041d13680
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-04-15 18:33:30 -0400

    Use git version of pairing-scheduler lib

--- project.clj
+++ project.clj
@@ -1,19 +1,13 @@
 (defproject dojo "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jarohen/chime "0.3.2"]
-                 [com.draines/postal "2.0.3"]
-                 [io.bloomventures/omni "0.26.2"]
-                 [io.bloomventures/commons "0.10.7"]
 
-                 [garden "1.3.10"]
-                 [re-frame "0.10.5"]
-                 [reagent "0.10.0"]
-                 [org.clojure/clojurescript "1.10.764"]
-                 [hiccup "1.0.5"]]
-
-  :plugins [[io.bloomventures/omni "0.25.4"]]
+  :plugins [[io.bloomventures/omni "0.25.4"]
+            [lein-tools-deps "0.4.5"]]
 
   :omni-config dojo.omni-config/omni-config
+
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+
+  :lein-tools-deps/config {:config-files [:install :user :project]}
 
   :main dojo.core
   :repl-options {:init-ns dojo.core}
