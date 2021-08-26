b6fcfe952b36d5d8cd5dd7b4f1a21eb6d0765f48
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-08-26 18:45:01 -0400

    Refactor routes to use TADA (#8)

--- src/dojo/core.clj
+++ src/dojo/core.clj
@@ -8,7 +8,15 @@
     [dojo.omni-config :refer [omni-config]]
     [dojo.seed :as seed]))
 
+(defn set-default-exception-handler
+  []
+  (Thread/setDefaultUncaughtExceptionHandler
+   (reify Thread$UncaughtExceptionHandler
+     (uncaughtException [_ thread ex]
+       (println ex "Uncaught exception " (.getName thread))))))
+
 (defn start! []
+  (set-default-exception-handler)
   (omni/start! omni/system (omni-config))
   (jobs.opt-in-email/schedule-email-job!)
   (jobs.match-email/schedule-email-job!))
