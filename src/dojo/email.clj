e3a1af6061fd7a46e9a5c514add8b157dcda5f92
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-03-10 23:53:48 +0000

    Include text/plain version in emails
    Co-authored-by: Darren Beattie <darren@fettle.ca>
    Co-authored-by: Don Stevenson <dsteven1@gmail.com>
    Co-authored-by: Niki Jiandani <niki.jiandani@gmail.com>

--- src/dojo/email.clj
+++ src/dojo/email.clj
@@ -4,9 +4,38 @@
     [hiccup.core :as hiccup]
     [dojo.config :refer [config]]))
 
+(defn strip-classes-and-ids [k]
+  (keyword (first (clojure.string/split (name k) #"[.#]"))))
+
+#_(strip-classes-and-ids :p.event)
+
+(defn textify [content]
+  ;; TODO should normalize all vector nodes to include {}
+  (clojure.walk/postwalk
+    (fn [node]
+      #_(println node)
+      (cond
+        (and (vector? node) (not (map-entry? node)))
+        (case (strip-classes-and-ids (first node))
+          :br
+          "\n"
+          :a
+          (str (clojure.string/join "" (drop 2 node)) " (" (:href (second node)) ")")
+          :p
+          (str "\n" (clojure.string/join "" (rest node)) "\n")
+          :div
+          (str (clojure.string/join "" (rest node)) "\n")
+          ; else
+          (clojure.string/join "" (rest node)))
+        (seq? node)
+        (clojure.string/join "" node)
+        :else
+        node))
+    content))
+
 (defn send!
   [{:keys [to subject body attachments]}]
-  (println body)
+  (println (textify body))
   (try
     (postal/send-message
       (:smtp-credentials @config)
@@ -15,7 +44,9 @@
        :subject subject
        :List-Unsubscribe "<mailto: unsubscribe@clojodojo.com?subject=unsubscribe>"
        :body (concat [{:type "text/html; charset=utf-8"
-                       :content (hiccup/html body)}]
+                       :content (hiccup/html body)}
+                      {:type "text/plain; charset=utf-8"
+                       :content (textify body)}]
                      attachments)})
     (catch com.sun.mail.util.MailConnectException _
       (println "Couldn't send email."))))
