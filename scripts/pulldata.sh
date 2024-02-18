220bffad872d0e21f30d6051f68c269c70f28a4d
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-02-18 14:51:28 -0500

    Fix pulldata script

--- scripts/pulldata.sh
+++ scripts/pulldata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 current_dir=$(dirname "$0")
-rsync -run -av --delete clojurecamp:/www/myclojurecamp/data/data "$current_dir"/../external
+rsync -av --progress --delete clojurecamp:/www/myclojurecamp/data/data/ "$current_dir"/../external/
