3ba98fe6339dad4598b07c33d751b964dc26527a
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-11-10 22:28:21 -0500

    Move migrations into their own file

---
+++ dev-src/migrations.clj
@@ -0,0 +1,79 @@
+(ns migrations)
+
+;; add :role/student to all users
+#_(->> (mycc.common.db/get-users)
+       (map (fn [u]
+              (assoc u :user/role :role/student)))
+       (map mycc.common.db/save-user!)
+       doall)
+
+;; add :user/created-at to all users, based on system file creation time
+#_(->> (mycc.common.db/get-users)
+       (remove :user/created-at)
+       (map (fn [{:user/keys [id] :as u}]
+              (assoc u :user/created-at
+                (->> (mycc.common.db/->path :user id)
+                     ;; using ext4, have creation date
+                     ;; if this didn't work, could fall back to modified date (%Y)
+                     (clojure.java.shell/sh ")stat" "-c" "%W" )
+                     :out
+                     clojure.string/trim
+                     (memfn Long.)
+                     (* 1000)
+                     (memfn java.util.Date.)))))
+       (map mycc.common.db/save-user!)
+       doall)
+
+#_(->> (mycc.common.db/get-users)
+       (map (fn [u]
+              (some-> u
+                      (assoc :user/pair-next-week? true)
+                      (update :user/pair-opt-in-history conj
+                              (mycc.common.date/next-monday))
+                      mycc.common.db/save-user!)))
+       (map mycc.common.db/save-user!)
+       doall)
+
+
+;; add :user/max-pair-same-user
+#_(->> (mycc.common.db/get-users)
+       (map (fn [u]
+              (assoc u :user/max-pair-same-user 2)))
+       (map mycc.common.db/save-user!)
+       doall)
+
+;; add empty :user/pair-opt-in-history
+#_(->> (mycc.common.db/get-users)
+       (map (fn [u]
+              (assoc u :user/pair-opt-in-history #{})))
+       (map mycc.common.db/save-user!)
+       doall)
+
+#_(->> (mycc.common.db/get-users)
+       (filter :user/pair-next-week?)
+       (map (fn [u]
+              (update u :user/pair-opt-in-history conj
+                      (mycc.common.date/next-monday))))
+       (map mycc.common.db/save-user!)
+       doall)
+
+
+
+;; add empty :user/primary-languages & :user/secondary-languages
+#_(->> (mycc.common.db/get-users)
+       (map (fn [u]
+              (-> u
+                  (update :user/primary-languages (fn [l]
+                                                    (or l #{})))
+                  (update :user/secondary-languages (fn [l]
+                                                    (or l #{})))))))
+
+#_(->> (mycc.common.db/get-users)
+       (map (fn [u]
+              (update u :user/user-pair-deny-list
+                      (fn [x]
+                        (if x
+                          x
+                          #{})))))
+       (map mycc.common.db/save-user!)
+       doall)
--- dev-src/scripts.clj
+++ dev-src/scripts.clj
@@ -1,64 +1,52 @@
 (ns scripts)
 
-;; add :role/student to all users
-#_(->> (mycc.common.db/get-users)
-       (map (fn [u]
-              (assoc u :user/role :role/student)))
-       (map mycc.common.db/save-user!)
-       doall)
+;; time zone checks
 
-;; add :user/created-at to all users, based on system file creation time
-#_(->> (mycc.common.db/get-users)
-       (remove :user/created-at)
-       (map (fn [{:user/keys [id] :as u}]
-              (assoc u :user/created-at
-                (->> (mycc.common.db/->path :user id)
-                     ;; using ext4, have creation date
-                     ;; if this didn't work, could fall back to modified date (%Y)
-                     (clojure.java.shell/sh "stat" "-c" "%W" )
-                     :out
-                     clojure.string/trim
-                     (memfn Long.)
-                     (* 1000)
-                     (memfn java.util.Date.)))))
-       (map mycc.common.db/save-user!)
-       doall)
+;; run Intl.supportedValuesOf("timeZone") in browser and save to tz.edn
 
-;; add :user/max-pair-same-user
-#_(->> (mycc.common.db/get-users)
-       (map (fn [u]
-              (assoc u :user/max-pair-same-user 2)))
-       (map mycc.common.db/save-user!)
-       doall)
+#_(clojure.set/difference (set (read-string (slurp "tz.edn")))
+                          (set (java.time.ZoneId/getAvailableZoneIds)))
 
-;; add empty :user/pair-opt-in-history
-#_(->> (mycc.common.db/get-users)
-       (map (fn [u]
-              (assoc u :user/pair-opt-in-history #{})))
-       (map mycc.common.db/save-user!)
-       doall)
-
-#_(->> (mycc.common.db/get-users)
-       (filter :user/pair-next-week?)
-       (map (fn [u]
-              (update u :user/pair-opt-in-history conj
-                      (mycc.common.date/upcoming-monday))))
-       (map mycc.common.db/save-user!)
-       doall)
+#_(keep (fn [x]
+          (if (try
+                (java.time.ZoneId/of x)
+                (catch Exception _
+                  false))
+            nil
+            x))
+        (set (read-string (slurp "tz.edn"))))
 
 ;; validate users
 #_(->> (mycc.common.db/get-users)
+       (filter (fn [u]
+                   (clojure.string/includes? (:user/email u)
+                                             "jf")))
        (map (fn [u]
-              (malli.error/humanize (malli.core/explain mycc.base.schema/User u))))
+              (if-let [e (malli.error/humanize (malli.core/explain mycc.base.schema/User u))]
+                [(:user/email u) e]
+                )))
        first)
 
-;; add empty :user/primary-languages & :user/secondary-languages
-#_(->> (mycc.common.db/get-users)
-       (map (fn [u]
-              (-> u
-                  (update :user/primary-languages (fn [l]
-                                                    (or l #{})))
-                  (update :user/secondary-languages (fn [l]
-                                                    (or l #{}))))))
-       (map mycc.common.db/save-user!)
-       doall)
+;; see what events will be created
+
+#_(let [date (tick.core/date "2024-04-01")]
+    (mycc.p2p.match-email-job/generate-schedule
+      (filter (fn [u]
+                (contains? (:user/pair-opt-in-history u) date))
+              (mycc.common.db/get-users))
+      date))
+
+;; availability in time zone for user
+#_(let [user-id #uuid "79af1d15-97ae-45c8-98ca-f95ec9118a19"
+        to-time-zone "America/Toronto"
+        monday (tick.core/date "2024-04-01")
+        user (mycc.common.db/get-user user-id)]
+    (->> (:user/availability user)
+         (filter (fn [[_ available?]] available?))
+         (map (fn [[day-and-hour _]]
+                (mycc.common.date/convert-time
+                  day-and-hour
+                  (:user/time-zone user)
+                  monday)))
+         (map (fn [t]
+                (tick.core/in t (tick.core/zone to-time-zone))))))
